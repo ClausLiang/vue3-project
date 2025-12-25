35141203ca33d7cec7b6b64c7f6c80a35af35548
clausliang <1475524294@qq.com>
2025-12-25 15:17:17 +0800

    feat: vue3项目初始化提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="top-header">
+    <span class="didit">You did it!</span>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+  <RouterView />
+</template>
+
+<style>
+.top-header {
+  margin: 0;
+}
+.top-header .didit {
+  font-size: 2em;
+}
+.top-header nav a {
+  margin-right: 5px;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    这是about页面
+  </div>
+</template>
