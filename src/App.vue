5e23fdab4b6931271d4f67d0d29f32d1ee91d38d
clausliang <1475524294@qq.com>
2025-12-31 11:39:03 +0800

    feat: 搭建壳子

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="top-header">
-    <span class="didit">You did it!</span>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </div>
   <RouterView />
 </template>
 
 <style>
-.top-header {
-  margin: 0;
-}
-.top-header .didit {
-  font-size: 2em;
-}
-.top-header nav a {
-  margin-right: 5px;
-}
 </style>
---
+++ src/components/layout/LayoutMain.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="layout-main">
+    <div class="layout-main-menu"></div>
+    <div class="layout-main-content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped lang="less">
+.layout-main {
+  display: flex;
+  height: 100vh;
+  .layout-main-menu {
+    width: 200px;
+    background-color: #f5f5f5;
+  }
+  .layout-main-content {
+    flex: 1;
+  }
+}
+</style>
