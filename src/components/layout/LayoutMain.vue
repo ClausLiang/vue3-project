d0791a99a21148cfa9c658c483f22bed30017a49
clausliang <1475524294@qq.com>
2026-01-04 16:34:01 +0800

    fix: 类型修复

--- src/components/layout/LayoutMain.vue
+++ src/components/layout/LayoutMain.vue
@@ -12,17 +12,19 @@
     </a-layout-content>
   </a-layout>
 </template>
-<script setup>
+<script setup lang="ts">
 import { reactive, h, watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import { HomeOutlined, BlockOutlined, SettingOutlined } from '@ant-design/icons-vue'
+import type { ItemType } from 'ant-design-vue'
+type Key = string | number
 const data = reactive({
   collapsed: false,
   selectedKeys: ['/home'],
-  openKeys: [],
+  openKeys: [] as Key[],
 })
 
-const items = reactive([
+const items: ItemType[] = reactive([
   { label: '首页', key: '/home', icon: () => h(HomeOutlined), path: '/home' },
   {
     label: '典型页面',
@@ -33,20 +35,21 @@ const items = reactive([
 ])
 const router = useRouter()
 // 点击跳转页面
-const gotoPage = (e) => {
+const gotoPage = (e: any) => {
   router.push(e.item.path)
 }
 // 获取父级key
-const getParentKey = (key) => {
+const getParentKey = (key: string) => {
   for (const item of items) {
-    if (item.children) {
+    if (item && 'children' in item && item.children) {
       for (const child of item.children) {
-        if (child.key === key) {
-          return [item.key]
+        if (child && child.key === key) {
+          return [item.key!]
         }
       }
     }
   }
+  return []
 }
 const route = useRoute()
 // 监听路由选中及展开菜单
@@ -54,7 +57,7 @@ watch(
   () => route,
   (newVal) => {
     data.selectedKeys = [newVal.path]
-    data.openKeys = getParentKey(newVal.path) || []
+    data.openKeys = getParentKey(newVal.path)
   },
   { immediate: true }
 )
